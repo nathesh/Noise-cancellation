343d09017987c0e60be61ba4a51cbbd09be79240
Chris Correll <chriscorrell@wireless-10-147-172-218.public.utexas.edu>
2015-03-10 23:53:10 -0500

    fixed A weighting debug FFT for right freq and reading

--- src/Weighting.c
+++ src/Weighting.c
@@ -7,7 +7,6 @@
 #define IMAG 0
 #define SAMPLE_RATE 8000
 #define F_RES SAMPLE_RATE/(2*FRAMES_PER_BUFFER)
-#define X 3.5041384E16
 #define Y1 20.598997*20.598997
 #define Y2 107.65265*107.65265
 #define Y3 737.86223*737.86223
@@ -22,9 +21,9 @@ float* A_compute_coeff() {
 	float* A = malloc(sizeof(float) * SAMPLE_RATE/(2*F_RES));
   for (i=0; i < FRAMES_PER_BUFFER; i++) {
        freq = i * F_RES;
-       A[i] = ((X *pow(freq,8))/ 
-              ((Y1 + pow(freq,2)) * (Y2 + pow(freq,2)) * (Y3 + pow(freq,2)) * (Y4 + pow(freq,2))));
-			 printf("freq:%3d  %12f \n",i*F_RES,A[i]);
+       A[i] = 20*log10(((Y4 *pow(freq,4))/ 
+              ((Y1 + pow(freq,2)) * sqrt(Y2 + pow(freq,2)) * sqrt(Y3 + pow(freq,2)) * (Y4 + pow(freq,2)))))+2;
+	//		 printf("freq:%3d  %12f \n",i*F_RES,A[i]);
 		}
 	return A;
 }
@@ -35,20 +34,29 @@ float* A_compute_coeff() {
 	float mag;
 	float sum = 0;
 	float* bands = malloc(sizeof(float) * NUM_BINS);
+ //init bands with 0's
+	for (i=0; i<NUM_BINS; i++){
+		bands[i] = 0;
+}
   //do this better where A is a global variable
 	float* A = A_compute_coeff();
 	for (i = 0; i < FRAMES_PER_BUFFER; i++) {
        mag = (float) sqrt(fftdata[i][REAL]*fftdata[i][REAL]+
                     fftdata[i][IMAG]*fftdata[i][IMAG]);
-       mag *= A[i];
-       bands[(int)ceil(i*F_RES/4000 * 10)] += mag;
-       sum += bands[(int)ceil(i*F_RES/4000 * 10)];
-	}
-
+       mag = 20*log10(mag);
+       mag += A[i];
+			// printf("%4.1f",mag);
+      //is there a better way to check if btw - and infinity
+			  if((-10000 < mag)&&(mag < 10000)){
+         //  printf("freq: %d  bin:%d mag:%4.1f dB \n",i*F_RES,(int)floor(i*F_RES*NUM_BINS*2/SAMPLE_RATE),mag);
+				   bands[(int)floor(i*F_RES*NUM_BINS*2/SAMPLE_RATE)] += mag;
+				   sum += bands[(int)floor(i*F_RES*NUM_BINS*2/SAMPLE_RATE)];
+				}
+}
+//	printf("%4.1f",sum);
 	for (i = 0; i < NUM_BINS; i++) {
-	     bands[i] /= sum;
-       bands[i] = 10*log10(bands[i]);
-	     printf("%4.1f dB \n",bands[i]);
+	     bands[i] /= FRAMES_PER_BUFFER/NUM_BINS;
+	    // printf("BAND:%d %4.1f dB \n",i,bands[i]);
 	}
 	return bands;
 }
